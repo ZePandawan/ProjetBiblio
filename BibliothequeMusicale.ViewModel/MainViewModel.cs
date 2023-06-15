626689e4c312864fa742cc2ff70ef7fd90a192aa
Vincent_Reix_54 <vincentreix54@gmail.com>
2023-06-15 11:14:45 +0200

    Enfin tous les fichiers ptn

---
+++ BibliothequeMusicale.ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace BibliothequeMusicale
+{
+    public interface IMainView
+    {
+        
+    }
+
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IMainView _view;
+
+
+
+        public MainViewModel(IMainView view)
+        {
+
+        }
+
+
+    }
+}
